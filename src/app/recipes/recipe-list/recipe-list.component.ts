3c164b2407f04956210607fa5ca016dede39a4ac
[Amaan09] <[mohammedamaan79@gmail.com]>
2020-10-27 12:46:13 +0530

    recipe items services done

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,26 +1,21 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
+
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'This is a test recipe','https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1035&q=80'),
-    new Recipe('Another Recipe', 'This is a test recipe', 'https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1035&q=80')
-  ]
+  recipes: Recipe[];
+  
+  constructor (private recipeService: RecipeService) { }
 
-  @Output() selectedRecipe = new EventEmitter<Recipe>();
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  afterRecipeClick(recipe: Recipe) {
-    this.selectedRecipe.emit(recipe);
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,16 @@
+import { EventEmitter } from '@angular/core';
+
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
+    private recipes: Recipe[] = [
+        new Recipe('Test Recipe', 'This is a test recipe', 'https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1035&q=80'),
+        new Recipe('Another Recipe', 'This is a test recipe', 'https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1035&q=80')
+    ]
+
+    getRecipes() {
+        return this.recipes.slice();
+    }
+}
