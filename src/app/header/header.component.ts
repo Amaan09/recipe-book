b2ad99bc4f3603245bdc1711cd67b9673c3b930e
[Amaan09] <[mohammedamaan79@gmail.com]>
2020-11-01 15:41:59 +0530

    done with routing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,18 +7,4 @@ import { Component, EventEmitter, Output } from '@angular/core';
 
 export class HeaderComponent {
     collapsed = true;
-    @Output() status = new EventEmitter<{recipeStatus: boolean, shoppingStatus: boolean}>();
-    onShowRecipes() {
-        this.status.emit({
-            recipeStatus: true,
-            shoppingStatus: false
-        });
-    }
-
-    onShowShopping() {
-        this.status.emit({
-            recipeStatus: false,
-            shoppingStatus: true
-        });
-    }
 }
--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -8,15 +7,11 @@ import { RecipeService } from '../../recipe.service';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
-
+  @Input() index: number;
   @Input() recipe: Recipe;
-  constructor(private recipeService: RecipeService) { }
+  constructor() { }
 
   ngOnInit(): void {
-  }
-
-  onItemClick() {
-    this.recipeService.recipeSelected.emit(this.recipe);
   }
 
 }
