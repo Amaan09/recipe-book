f31e0e59894c99e5306f5d2185ab84e578765825
[Amaan09] <[mohammedamaan79@gmail.com]>
2020-11-08 14:19:20 +0530

    Added Subject

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
-import { Recipe } from './recipe.model';
 import { RecipeService } from './recipe.service';
 
 @Component({
@@ -12,16 +10,9 @@ import { RecipeService } from './recipe.service';
 
 export class RecipesComponent implements OnInit {
 
-  currRecipe: Recipe;
-
-  constructor(private recipeService: RecipeService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.recipeService.recipeSelected.subscribe(
-      (recipe: Recipe) => {
-        this.currRecipe = recipe;
-      }
-    )
   }
 
 }
