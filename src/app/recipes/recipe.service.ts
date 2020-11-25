63217551241f56a71633c215601943e840fefd24
[Amaan09] <[mohammedamaan79@gmail.com]>
2020-11-25 14:13:04 +0530

    made changes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,29 +10,36 @@ export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
     ingredient: Ingredient;
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            'Burger with Fries',
-            'This is a tasty Burger with good fries', 
-            'https://images.unsplash.com/photo-1460306855393-0410f61241c7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1052&q=80',
-            [
-                new Ingredient('Mutton', 1),
-                new Ingredient('Fries', 25)
-            ]   
-        ),
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'Burger with Fries',
+    //         'This is a tasty Burger with good fries', 
+    //         'https://images.unsplash.com/photo-1460306855393-0410f61241c7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1052&q=80',
+    //         [
+    //             new Ingredient('Mutton', 1),
+    //             new Ingredient('Fries', 25)
+    //         ]   
+    //     ),
         
-        new Recipe(
-            'BBQ Pizza', 
-            'This is a deliocious Pizza from Italy', 
-            'https://images.unsplash.com/photo-1458642849426-cfb724f15ef7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
-            [
-                new Ingredient('Chicken', 1),
-                new Ingredient('Oregano', 5)
-            ]
-        )
-    ]
+    //     new Recipe(
+    //         'BBQ Pizza', 
+    //         'This is a deliocious Pizza from Italy', 
+    //         'https://images.unsplash.com/photo-1458642849426-cfb724f15ef7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+    //         [
+    //             new Ingredient('Chicken', 1),
+    //             new Ingredient('Oregano', 5)
+    //         ]
+    //     )
+    // ]
+
+    private recipes: Recipe[] = [];
 
     constructor(private shoppingListService: ShoppingListService) {}
+
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
+    }
 
     getRecipes() {
         return this.recipes.slice();
